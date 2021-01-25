d3e86e85135e5a44512d6ca6a8a47866f5ee6e22
Morten Sørensen <morten@moso.io>
2021-01-25 09:16:17 +0100

    Updated main package

--- packages/all/index.js
+++ packages/all/index.js
@@ -1,6 +1,9 @@
 module.exports = {
     extends: [
+        '@moso/eslint-config/fifa',
         '@moso/eslint-config-nuxt',
+        '@moso/eslint-config/react',
+        '@moso/eslint-config/typescript',
         '@moso/eslint-config-vue'
     ]
 }
