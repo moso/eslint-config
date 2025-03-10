b90d6d3d3f3ed3364e10b48ab93241226825ac0f
Morten Sørensen <morten@moso.dev>
2025-03-10 09:55:59 +0100

    Updated internal utilities

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
-import { OptionsConfig } from '@/types';
-import type { Awaitable } from '@/types';
+import process from 'node:process';
+
+import type { Awaitable, OptionsConfig, TypedFlatConfigItem } from './types';
 
 export const parserPlain = {
     meta: {
@@ -12,7 +13,6 @@ export const parserPlain = {
             loc: { end: code.length, start: 0 },
             range: [0, code.length],
             tokens: [],
-            type: 'Program',
         },
         scopeManager: null,
         services: { isPlain: true },
@@ -22,13 +22,16 @@ export const parserPlain = {
     }),
 };
 
+export const combine = async (...configs: Awaitable<TypedFlatConfigItem | TypedFlatConfigItem[]>[]): Promise<TypedFlatConfigItem[]> => {
+    const resolved = await Promise.all(configs);
+    return resolved.flat();
+};
+
 export const toArray = <T>(value: T | T[]): T[] => {
     return Array.isArray(value) ? value : [value];
-}
+};
 
-export const interopDefault = async <T>(
-    r: Awaitable<T>
-): Promise<T extends { default: infer U } ? U : T> => {
+export const interopDefault = async <T>(r: Awaitable<T>): Promise<T extends { default: infer U } ? U : T> => {
     const resolved = await r;
     return (resolved as any).default || resolved;
 };
@@ -41,7 +44,7 @@ export const resolveSubOptions = <K extends keyof OptionsConfig>(
 ): ResolvedOptions<OptionsConfig[K]> => {
     return typeof options[key] === 'boolean'
         ? {} as any
-        : options[key] || {};
+        : options[key] || {} as any;
 };
 
 export const getOverrides = <K extends keyof OptionsConfig>(
@@ -52,7 +55,29 @@ export const getOverrides = <K extends keyof OptionsConfig>(
     return {
         ...(options.overrides as any)?.[key],
         ...'overrides' in sub
-        ? sub.overrides
-        : {},
+            ? sub.overrides
+            : {},
     };
+};
+
+export const isInGitHooksOrLintStaged = (): boolean => {
+    return !!(false
+        || process.env.GIT_PARAMS
+        || process.env.VSCODE_GIT_COMMAND
+        || process.env.npm_lifecycle_script?.startsWith('lint-staged')
+    );
+};
+
+export const isInEditorEnv = (): boolean => {
+    if (process.env.CI) return false;
+
+    if (isInGitHooksOrLintStaged()) return false;
+
+    return !!(false
+        || process.env.VSCODE_PID
+        || process.env.VSCODE_CWD
+        || process.env.JETBRAINS_IDE
+        || process.env.VIM
+        || process.env.NVIM
+    );
 };
