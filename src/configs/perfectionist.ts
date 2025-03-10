63962a186840de6ea6cac1937e708e9224b65b94
Morten Sørensen <morten@moso.dev>
2025-03-10 09:59:22 +0100

    Updated Perfectionist config

--- src/configs/perfectionist.ts
+++ src/configs/perfectionist.ts
@@ -1,13 +1,24 @@
-import { interopDefault } from '@/utils';
+import { interopDefault } from '../utils';
 
-import type { TypedFlatConfigItem } from '@/types';
+import type { OptionsOverrides, TypedFlatConfigItem } from '../types';
+
+export interface PerfectionistOptions extends OptionsOverrides {
+    lessOpinionated?: boolean;
+};
 
 /**
  * Optional perfectionist plugin for props and items sorting.
  *
- * @see https://github.com/azat-io/eslint-plugin-perfectionist
+ * @see https://perfectionist.dev
  */
-export const perfectionist = async (): Promise<TypedFlatConfigItem[]> => {
+export const perfectionist = async (options: PerfectionistOptions = {}): Promise<TypedFlatConfigItem[]> => {
+    const {
+        lessOpinionated = false,
+        overrides = {},
+    } = {
+        ...options,
+    };
+
     const perfectionistPlugin = await interopDefault(import('eslint-plugin-perfectionist'));
 
     return [
@@ -15,6 +26,36 @@ export const perfectionist = async (): Promise<TypedFlatConfigItem[]> => {
             name: 'moso/perfectionist/setup',
             plugins: {
                 perfectionist: perfectionistPlugin,
+            },
+            rules: {
+                ...(lessOpinionated
+                    ? {}
+                    : {
+                        'perfectionist/sort-exports': ['error', { order: 'asc', type: 'natural' }],
+                        'perfectionist/sort-imports': ['error', {
+                            groups: [
+                                'builtin',
+                                'external',
+                                'internal',
+                                ['parent', 'sibling', 'index'],
+                                'side-effect',
+                                'object',
+                                'unknown',
+                                'builtin-type',
+                                'external-type',
+                                'type',
+                                ['parent-type', 'sibling-type', 'index-type', 'internal-type'],
+                            ],
+                            newlinesBetween: 'ignore',
+                            order: 'asc',
+                            type: 'natural',
+                        }],
+                        'perfectionist/sort-named-exports': ['error', { order: 'asc', type: 'natural' }],
+                        'perfectionist/sort-named-imports': ['error', { order: 'asc', type: 'natural' }],
+                    }
+                ),
+
+                ...overrides,
             },
         },
     ];
