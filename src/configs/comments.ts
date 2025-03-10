4c424d67d76d0a4e29125b65519c6666389f5bf2
Morten Sørensen <morten@moso.dev>
2025-03-10 09:56:13 +0100

    Updated comments config

--- src/configs/comments.ts
+++ src/configs/comments.ts
@@ -1,21 +1,21 @@
-import { interopDefault } from '@/utils';
+import { interopDefault } from '../utils';
 
-import type { TypedFlatConfigItem } from '@/types';
+import type { TypedFlatConfigItem } from '../types';
 
 export const comments = async (): Promise<TypedFlatConfigItem[]> => {
-    const eslintComments = await interopDefault(import('eslint-plugin-eslint-comments'));
+    const eslintComments = await interopDefault(import('@eslint-community/eslint-plugin-eslint-comments'));
 
     return [
         {
             name: 'moso/eslint-comments/rules',
             plugins: {
-                comments: eslintComments,
+                '@eslint-community/eslint-comments': eslintComments,
             },
             rules: {
-                'comments/no-aggregating-enable': 'error',
-                'comments/no-duplicate-disable': 'error',
-                'comments/no-unlimited-disable': 'error',
-                'comments/no-unused-enable': 'error',
+                '@eslint-community/eslint-comments/no-aggregating-enable': 'error',
+                '@eslint-community/eslint-comments/no-duplicate-disable': 'error',
+                '@eslint-community/eslint-comments/no-unlimited-disable': 'error',
+                '@eslint-community/eslint-comments/no-unused-enable': 'error',
             },
         },
     ];
