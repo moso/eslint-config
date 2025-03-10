436e0d3551f9026dd065b9485b66cfeeb33a6059
Morten Sørensen <morten@moso.dev>
2025-03-10 09:56:47 +0100

    Updated ignores config

--- src/configs/ignores.ts
+++ src/configs/ignores.ts
@@ -1,12 +1,15 @@
-import { GLOB_EXCLUDE } from '@/globs';
+import { GLOB_EXCLUDE } from '../globs';
 
-import type { TypedFlatConfigItem } from '@/types';
+import type { TypedFlatConfigItem } from '../types';
 
-export const ignores = async (): Promise<TypedFlatConfigItem[]> => {
+export const ignores = (userIgnores: string[] = []): TypedFlatConfigItem[] => {
     return [
         {
             name: 'moso/ignores',
-            ignores: GLOB_EXCLUDE,
+            ignores: [
+                ...GLOB_EXCLUDE,
+                ...userIgnores,
+            ],
         },
     ];
 };
