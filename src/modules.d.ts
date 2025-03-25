0dc54fc9b469a6d081aa598f32714e7a29288c22
Morten Sørensen <morten@moso.dev>
2025-03-25 07:51:43 +0100

    Clean up of unnecessary module definitions

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,13 +1,3 @@
 // Modules that may or may not need types
-declare module '@antfu/eslint-plugin';
 declare module '@eslint-community/eslint-plugin-eslint-comments';
-declare module 'eslint-plugin-import-x';
-declare module 'eslint-plugin-markdown';
 declare module 'eslint-plugin-no-only-tests';
-declare module 'eslint-plugin-perfectionist';
-declare module 'eslint-plugin-react';
-declare module 'eslint-plugin-react-hooks';
-declare module 'eslint-plugin-react-refresh';
-declare module 'eslint-plugin-unicorn';
-declare module 'eslint-plugin-unused-imports';
-declare module 'eslint-plugin-vue';
