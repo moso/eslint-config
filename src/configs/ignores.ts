081bd934ce820d46443b7cc685fcea0815c982d5
Morten Sørensen <morten@moso.dev>
2025-09-23 08:08:43 +0200

    Fixed ignores rules

--- src/configs/ignores.ts
+++ src/configs/ignores.ts
@@ -1,15 +1,66 @@
+import assert from 'node:assert/strict';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
 import { GLOB_EXCLUDE } from '../globs';
+import { loadPackages } from '../utils';
 
-import type { TypedFlatConfigItem } from '../types';
+import type {
+    OptionsIgnoreFiles,
+    OptionsIgnores,
+    OptionsProjectRoot,
+    TypedFlatConfigItem,
+} from '../types';
 
-export const ignores = (userIgnores: string[] = []): TypedFlatConfigItem[] => {
+export const ignores = async (
+    options: Readonly<
+        OptionsIgnoreFiles &
+        OptionsProjectRoot &
+        { ignores: OptionsIgnores }
+    >,
+): Promise<TypedFlatConfigItem[]> => {
+    const {
+        ignoreFiles,
+        ignores: ignoreOptions,
+        projectRoot,
+    } = options;
+
+    const includeIgnoreFile = ignoreFiles.length === 0
+        ? undefined
+        : await loadPackages(['@eslint/compat']).then(([x]) => (x as typeof import('@eslint/compat')).includeIgnoreFile);
+
+    const [extend, files] = Array.isArray(ignoreOptions)
+        ? [true, ignoreOptions]
+        : [ignoreOptions.extend, ignoreOptions.files];
+
+    const ignoreFileConfigs = typeof projectRoot === 'string'
+        ? (await Promise.all(
+            ignoreFiles.map(async (file) => {
+                assert(includeIgnoreFile !== undefined);
+                const filePath = path.resolve(projectRoot, file);
+                return fs
+                    .access(filePath)
+                    .then(() => includeIgnoreFile(filePath))
+                    .catch(() => {
+                        console.warn(`Ignore file "${filePath} not found.`);
+                        return null;
+                    });
+            }),
+        ))
+        : undefined;
+
+    const ignoreConfig = {
+        name: 'eslint/ignores',
+        ignores: extend
+            ? [...GLOB_EXCLUDE, ...files]
+            : [...files],
+    };
+
     return [
-        {
-            name: 'moso/ignores',
-            ignores: [
-                ...GLOB_EXCLUDE,
-                ...userIgnores,
-            ],
-        },
+        ignoreConfig,
+        ...((ignoreFileConfigs
+            ? [...ignoreFileConfigs.filter(<T>(v: null | T): v is T => v !== null)]
+            : []) satisfies TypedFlatConfigItem[]
+        ),
     ];
 };
