2fbbc6c86c5f3ad6a1a0decd7a1389377239a835
Morten Sørensen <morten@moso.dev>
2025-09-23 08:10:04 +0200

    Updated JSDoc rules

--- src/configs/jsdoc.ts
+++ src/configs/jsdoc.ts
@@ -1,41 +1,74 @@
-import { interopDefault } from '../utils';
+import { loadPackages, memoize } from '../utils';
 
-import type { OptionsStylistic, TypedFlatConfigItem } from '../types';
+import type { ESLint } from 'eslint';
 
-export const jsdoc = async (options: OptionsStylistic = {}): Promise<TypedFlatConfigItem[]> => {
-    const { stylistic = true } = options;
+import type {
+    OptionsOverrides,
+    RequiredOptionsStylistic,
+    TypedFlatConfigItem,
+} from '../types';
 
-    const jsdocPlugin = await interopDefault(import('eslint-plugin-jsdoc'));
+export const jsdoc = async (
+    options: Readonly<OptionsOverrides & Required<RequiredOptionsStylistic>>,
+): Promise<TypedFlatConfigItem[]> => {
+    const { overrides, stylistic } = options;
+
+    const [jsdocPlugin] = (await loadPackages(['eslint-plugin-jsdoc'])) as [ESLint.Plugin];
+
+    const stylisticEnabled = stylistic === false ? 'off' : 'error';
 
     return [
         {
-            name: 'moso/jsdoc/rules',
+            name: 'moso/jsdoc',
             plugins: {
-                jsdoc: jsdocPlugin,
+                'jsdoc': memoize(jsdocPlugin, 'eslint-plugin-jsdoc'),
             },
             rules: {
                 'jsdoc/check-access': 'warn',
-                'jsdoc/check-param-names': 'warn',
-                'jsdoc/check-property-names': 'warn',
-                'jsdoc/check-types': 'warn',
+                'jsdoc/check-alignment': stylisticEnabled,
+                'jsdoc/check-indentation': 'warn',
+                'jsdoc/check-line-alignment': 'error',
+                'jsdoc/check-param-names': 'error',
+                'jsdoc/check-property-names': 'error',
+                'jsdoc/check-types': 'error',
                 'jsdoc/empty-tags': 'warn',
                 'jsdoc/implements-on-classes': 'warn',
+                'jsdoc/multiline-blocks': stylisticEnabled,
+                'jsdoc/no-bad-blocks': [
+                    'warn',
+                    {
+                        ignore: [
+                            'ts-check',
+                            'ts-expect-error',
+                            'ts-ignore',
+                            'ts-nocheck',
+                            'vue-ignore',
+                        ],
+                    },
+                ],
                 'jsdoc/no-defaults': 'warn',
-                'jsdoc/no-multi-asterisks': 'warn',
-                'jsdoc/require-param-name': 'warn',
-                'jsdoc/require-property': 'warn',
+                'jsdoc/no-undefined-types': ['warn', { disableReporting: true }],
+                'jsdoc/require-asterisk-prefix': 'warn',
+                'jsdoc/require-description': 'warn',
+                'jsdoc/require-hyphen-before-param-description': 'warn',
+                'jsdoc/require-jsdoc': 'off',
+                'jsdoc/require-param-name': 'error',
                 'jsdoc/require-property-description': 'warn',
                 'jsdoc/require-property-name': 'warn',
-                'jsdoc/require-returns-check': 'warn',
-                'jsdoc/require-returns-description': 'warn',
-                'jsdoc/require-yields-check': 'warn',
+                'jsdoc/require-returns-check': 'error',
+                'jsdoc/require-throws': 'warn',
+                'jsdoc/require-yields-check': 'error',
+                'jsdoc/tag-lines': [
+                    'warn',
+                    'never',
+                    {
+                        applyToEndTag: false,
+                        startLines: 1,
+                        tags: { example: { lines: 'always' } },
+                    },
+                ],
 
-                ...stylistic
-                    ? {
-                        'jsdoc/check-alignment': 'warn',
-                        'jsdoc/multiline-blocks': 'warn',
-                    }
-                    : {},
+                ...overrides,
             },
         },
     ];
