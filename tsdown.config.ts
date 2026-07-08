060cd81038110935378204478793d7b835483b0e
Morten Sørensen <morten@moso.dev>
2026-07-08 19:13:11 +0200

    Adjusted tsdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'tsdown';
+import type { UserConfig } from 'tsdown';
 
-export default defineConfig({
+const defaultConfig: UserConfig = defineConfig({
+    deps: {
+        neverBundle: [/^@typescript-eslint\//u, /^typescript\//u],
+    },
+    dts: true,
     entry: ['src/index.ts'],
     exports: true,
     format: ['esm'],
     shims: true,
-    inlineOnly: false,
 });
+
+export default defaultConfig;
