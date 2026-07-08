79a1a87b06fc769feabbd065861bfcf75b930d0e
Morten Sørensen <morten@moso.dev>
2026-07-08 19:31:22 +0200

    Refactored JSX config, added proper a11y

--- src/configs/jsx.ts
+++ src/configs/jsx.ts
@@ -1,10 +1,6 @@
-import {
-    ensurePackages,
-    loadPackages,
-    memoize,
-} from '../utils';
+import assert from 'node:assert/strict';
 
-import type { ESLint } from 'eslint';
+import { loadPackages, memoize } from '../utils';
 
 import type {
     OptionsFiles,
@@ -30,11 +26,9 @@ export const jsx = async (
         stylistic,
     } = options;
 
-    if (a11y) await ensurePackages(['eslint-plugin-jsx-a11y']);
-
-    const jsxA11yPlugin = a11y
-        ? (await loadPackages(['eslint-plugin-jsx-a11y'])) as [ESLint.Plugin]
-        : undefined;
+    const [jsxA11yPlugin] = a11y
+        ? ((await loadPackages(['eslint-plugin-jsx-a11y'])) as [typeof import('eslint-plugin-jsx-a11y')])
+        : [undefined];
 
     const stylisticEnabled = stylistic !== false;
 
@@ -71,72 +65,40 @@ export const jsx = async (
                 ...overrides,
             },
         },
-        ...((a11y
+        ...((jsxA11yPlugin
             ? [
                 {
                     name: 'moso/jsx/a11y',
                     plugins: {
-                        'jsx-a11y': memoize(jsxA11yPlugin as ESLint.Plugin, 'eslint-plugin-jsx-a11y'),
+                        'jsx-a11y': memoize(jsxA11yPlugin, 'eslint-plugin-jsx-a11y'),
                     },
                     rules: {
-                        // Recommended rules from eslint-plugin-jsx-a11y, manually migrated
-                        'jsx-a11y/alt-text': 'error',
-                        'jsx-a11y/anchor-ambiguous-text': 'off',
-                        'jsx-a11y/anchor-has-content': 'error',
-                        'jsx-a11y/anchor-is-valid': 'error',
-                        'jsx-a11y/aria-activedescendant-has-tabindex': 'error',
-                        'jsx-a11y/aria-props': 'error',
-                        'jsx-a11y/aria-proptypes': 'error',
-                        'jsx-a11y/aria-role': 'error',
-                        'jsx-a11y/aria-unsupported-elements': 'error',
-                        'jsx-a11y/autocomplete-valid': 'error',
-                        'jsx-a11y/click-events-have-key-events': 'error',
-                        'jsx-a11y/control-has-associated-label': [
-                            'off',
-                            {
-                                ignoreElements: [
-                                    'audio',
-                                    'canvas',
-                                    'embed',
-                                    'input',
-                                    'textarea',
-                                    'tr',
-                                    'video',
-                                ],
-                                ignoreRoles: [
-                                    'grid',
-                                    'listbox',
-                                    'menu',
-                                    'menubar',
-                                    'radiogroup',
-                                    'row',
-                                    'tablist',
-                                    'toolbar',
-                                    'tree',
-                                    'treegrid',
-                                ],
-                                includeRoles: ['alert', 'dialog'],
-                            },
-                        ],
-                        'jsx-a11y/heading-has-content': 'error',
-                        'jsx-a11y/html-has-lang': 'error',
-                        'jsx-a11y/iframe-has-title': 'error',
-                        'jsx-a11y/img-redundant-alt': 'error',
-                        'jsx-a11y/interactive-supports-focus': [
-                            'error',
-                            {
-                                tabbable: [
-                                    ...(lessOpinionated
-                                        ? [
-                                            'button',
-                                            'checkbox',
-                                            'link',
-                                            'searchbox',
-                                            'spinbutton',
-                                            'switch',
-                                            'textbox',
-                                        ]
-                                        : [
+                        ...(lessOpinionated
+                            ? {
+                                ...(assert.ok(!Array.isArray(jsxA11yPlugin.flatConfigs.recommended)),
+                                jsxA11yPlugin.flatConfigs.recommended.rules),
+                            }
+                            : {
+                                // Minimal rules, inspired by SukkaW
+                                // @see https://github.com/SukkaW/eslint-config-sukka/tree/master/packages/eslint-plugin-react-jsx-a11y
+                                'jsx-a11y/alt-text': ['warn', { elements: ['img'], img: ['Image'] }],
+                                'jsx-a11y/aria-props': 'warn',
+                                'jsx-a11y/aria-proptypes': 'warn',
+                                'jsx-a11y/aria-role': 'warn',
+                                'jsx-a11y/aria-unsupported-elements': 'warn',
+                                'jsx-a11y/iframe-has-title': 'warn',
+                                'jsx-a11y/no-access-key': 'warn',
+                                'jsx-a11y/no-static-element-interactions': 'warn',
+                                'jsx-a11y/role-has-required-aria-props': 'warn',
+                                'jsx-a11y/role-supports-aria-props': 'warn',
+                                'jsx-a11y/tabindex-no-positive': 'warn',
+
+                                // Opinionated additions
+                                'jsx-a11y/anchor-ambiguous-text': 'off',
+                                'jsx-a11y/interactive-supports-focus': [
+                                    'error',
+                                    {
+                                        tabbable: [
                                             'button',
                                             'checkbox',
                                             'link',
@@ -146,128 +108,20 @@ export const jsx = async (
                                             'spinbutton',
                                             'switch',
                                             'textbox',
-                                        ]),
+                                        ],
+                                    },
                                 ],
-                            },
-                        ],
-                        'jsx-a11y/label-has-associated-control': 'error',
-                        'jsx-a11y/label-has-for': 'off',
-                        'jsx-a11y/media-has-caption': 'error',
-                        'jsx-a11y/mouse-events-have-key-events': 'error',
-                        'jsx-a11y/no-access-key': 'error',
-                        'jsx-a11y/no-autofocus': 'error',
-                        'jsx-a11y/no-distracting-elements': 'error',
-                        'jsx-a11y/no-interactive-element-to-noninteractive-role': [
-                            'error',
-                            ...(lessOpinionated
-                                ? [{
-                                    tr: ['none', 'presentation'],
-                                    canvas: ['img'],
-                                }]
-                                : []),
-                        ],
-                        'jsx-a11y/no-noninteractive-element-interactions': [
-                            'error',
-                            ...(lessOpinionated
-                                ? [{
-                                    handlers: [
-                                        'onClick',
-                                        'onError',
-                                        'onLoad',
-                                        'onMouseDown',
-                                        'onMouseUp',
-                                        'onKeyPress',
-                                        'onKeyDown',
-                                        'onKeyUp',
-                                    ],
-                                    alert: [
-                                        'onKeyUp',
-                                        'onKeyDown',
-                                        'onKeyPress',
-                                    ],
-                                    body: ['onError', 'onLoad'],
-                                    dialog: [
-                                        'onKeyUp',
-                                        'onKeyDown',
-                                        'onKeyPress',
-                                    ],
-                                    iframe: ['onError', 'onLoad'],
-                                    img: ['onError', 'onLoad'],
-                                }]
-                                : [{
-                                    body: ['onError', 'onLoad'],
-                                    iframe: ['onError', 'onLoad'],
-                                    img: ['onError', 'onLoad'],
-                                }]),
-                        ],
-                        'jsx-a11y/no-noninteractive-element-to-interactive-role': [
-                            'error',
-                            ...(lessOpinionated
-                                ? [{
-                                    ul: [
-                                        'listbox',
-                                        'menu',
-                                        'menubar',
-                                        'radiogroup',
-                                        'tablist',
-                                        'tree',
-                                        'treegrid',
-                                    ],
-                                    ol: [
-                                        'listbox',
-                                        'menu',
-                                        'menubar',
-                                        'radiogroup',
-                                        'tablist',
-                                        'tree',
-                                        'treegrid',
-                                    ],
-                                    li: [
-                                        'menuitem',
-                                        'menuitemradio',
-                                        'menuitemcheckbox',
-                                        'option',
-                                        'row',
-                                        'tab',
-                                        'treeitem',
-                                    ],
-                                    table: ['grid'],
-                                    td: ['gridcell'],
-                                    fieldset: ['radiogroup', 'presentation'],
-                                }]
-                                : []),
-                        ],
-                        'jsx-a11y/no-noninteractive-tabindex': [
-                            'error',
-                            ...(lessOpinionated
-                                ? [{
-                                    tags: [],
-                                    roles: ['tabpanel'],
-                                    allowExpressionValues: true,
-                                }]
-                                : []),
-                        ],
-                        'jsx-a11y/no-redundant-roles': 'error',
-                        'jsx-a11y/no-static-element-interactions': [
-                            'error',
-                            ...(lessOpinionated
-                                ? [{
-                                    allowExpressionValues: true,
-                                    handlers: [
-                                        'onClick',
-                                        'onMouseDown',
-                                        'onMouseUp',
-                                        'onKeyPress',
-                                        'onKeyDown',
-                                        'onKeyUp',
-                                    ],
-                                }]
-                                : []),
-                        ],
-                        'jsx-a11y/role-has-required-aria-props': 'error',
-                        'jsx-a11y/role-supports-aria-props': 'error',
-                        'jsx-a11y/scope': 'error',
-                        'jsx-a11y/tabindex-no-positive': 'error',
+                                'jsx-a11y/label-has-for': 'off',
+                                'jsx-a11y/no-noninteractive-element-interactions': [
+                                    'error',
+                                    {
+                                        body: ['onError', 'onLoad'],
+                                        iframe: ['onError', 'onLoad'],
+                                        img: ['onError', 'onLoad'],
+                                    },
+                                ],
+                            }
+                        ),
 
                         ...overridesA11y,
                     },
